17a4ffba60ac3b2447fbde3f7cbf288af22ae9bf
Carlos Villamil <carlosvillamilgrisales@gmail.com>
2024-10-02 20:29:33 -0500

    Initial commit

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,11 @@
+@props(['active'])
+
+@php
+$classes = ($active ?? false)
+            ? 'inline-flex items-center px-1 pt-1 border-b-4 border-white text-sm font-medium leading-5 text-white focus:outline-none focus:border-white transition duration-150 ease-in-out'
+            : 'inline-flex items-center px-1 pt-1 border-b-4 border-transparent text-sm font-medium leading-5 text-white hover:text-gray-200 hover:border-white focus:outline-none focus:text-gray-200 focus:border-white transition duration-150 ease-in-out';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
